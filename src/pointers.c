ef5d91984dfe6110cb8b434bdaf2c1050ad25ac2
KangWon LEE <kangwon@gmail.com>
2025-06-29 23:09:04 +0900

    style : change spaces around `*`

--- src/pointers.c
+++ src/pointers.c
@@ -3,8 +3,8 @@
 #include "pointers.h"
 #include <stdlib.h>
 
-int* allocate_integer(int value) {
-    int* ptr = (int*)malloc(sizeof(int));
+int *allocate_integer(int value) {
+    int *ptr = (int *)malloc(sizeof(int));
     if (ptr == NULL) {
         // Handle allocation failure (e.g., return NULL or exit)
         return NULL; // Or handle differently
@@ -13,7 +13,7 @@ int* allocate_integer(int value) {
     return ptr;
 }
 
-void deallocate_integer(int* ptr) {
+void deallocate_integer(int *ptr) {
     free(ptr);
 }
 
